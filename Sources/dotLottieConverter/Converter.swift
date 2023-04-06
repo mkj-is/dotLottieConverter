8307c4eaebf6f6a7784eaa68da98d5d077abeb44
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2023-04-06 21:33:52 +0200

    Add output folder and verbose parameters

--- Sources/dotLottieConverter/Converter.swift
+++ Sources/dotLottieConverter/Converter.swift
@@ -11,14 +11,29 @@ struct Converter: AsyncParsableCommand {
     @Argument(help: "List of Lottie JSON files to be converted to .lottie")
     private var files: [String]
 
+    @Option(help: "Output folder")
+    private var output: String?
+
+    @Flag
+    private var verbose: Bool = false
+
     func run() async throws {
+        let outputFolder = output.flatMap { URL(fileURLWithPath: $0, isDirectory: true) }
         for file in files {
-            let url = URL(filePath: file)
+            let url = URL(fileURLWithPath: file)
             let result = await DotLottieCreator(animationUrl: url).create()
-            if let result {
-                print(result)
-            } else {
+            guard let result else {
                 throw ConverterError.fileNotDecoded(file: file)
+            }
+
+            if let outputFolder {
+                let destinationURL = outputFolder.appendingPathComponent(result.lastPathComponent)
+                try FileManager.default.moveItem(at: result, to: destinationURL)
+                if verbose {
+                    print(destinationURL)
+                }
+            } else if verbose {
+                print(result)
             }
         }
     }
